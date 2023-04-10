2001c7a48dbf0e1568647a30355f225df5f27823
Faisal <faisalimtiaz@logicon.com.pk>
2023-04-10 15:00:43 +0500

    removed dist

--- dist/services/user.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const userService = {
-    createUser(req, res) {
-        console.log("CREATING USER...");
-        res.send("User Created!");
-    },
-};
-exports.default = userService;
